6ebec9ff0e460650a4942159f1e814c4195161aa
pulkitgulati <pulkit.gemini@gmail.com>
2019-06-06 16:27:14 +0530

    maintain the local state of the drop-down with i18n lang.

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,9 @@ import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
-import Select from "react-select";
-import { options } from "../config/options";
-import "../config/i18n";
-import { withNamespaces } from "react-i18next";
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lang: options[0]
-    };
-  }
-
-  changeLang = lang => {
-    const { i18n } = this.props;
-    const { value } = lang;
-    this.setState({
-      lang
-    });
-    i18n.changeLanguage(value);
-  };
-
   render() {
-    const { lang } = this.state;
     //const { t } = this.props;
 
     return (
@@ -40,17 +19,9 @@ export class App extends Component {
           <Route path="/about" component={AboutPage} />
           <Route component={PageNotFound} />
         </Switch>
-        <br />
-        <Select
-          defaultValue={options[0]}
-          options={options}
-          value={lang}
-          onChange={this.changeLang}
-          className="App-Select"
-        />
       </div>
     );
   }
 }
 
-export default withNamespaces("translations")(App);
+export default App;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 //import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import Select from "react-select";
+import { options } from "../../config/options";
+import "../../config/i18n";
+import { withNamespaces } from "react-i18next";
 
-const HomePage = () => (
-  <div className="jumbotron">
-    <h1>Courses Administration</h1>
-    <Link to="about" className="btn btn-primary btn-lg">
-      Learn more
-    </Link>
-  </div>
-);
+export class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    console.log("inside constructor");
+    //console.log(props);
+    this.state = {
+      lang: options[0]
+    };
+  }
 
-export default HomePage;
+  changeLang = lang => {
+    const { i18n } = this.props;
+    const { value } = lang;
+    this.setState({
+      lang
+    });
+    console.log("changeLang", lang);
+    i18n.changeLanguage(value);
+  };
+
+  componentDidMount() {
+    console.log("component did mount");
+    this.setState(
+      {
+        lang: options.filter(item => item.value === this.props.i18n.language)
+      },
+      () => console.log(this.state)
+    );
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
+    if (this.props.i18n.language) {
+      this.setState(
+        {
+          lang: options.filter(item => item.value === this.props.i18n.language)
+        },
+        () => console.log(this.state)
+      );
+    }
+  }
+
+  render() {
+    const { lang } = this.state;
+    console.log(this.state.lang);
+    return (
+      <React.Fragment>
+        <div className="jumbotron">
+          <h1>Courses Administration</h1>
+          <Link to="about" className="btn btn-primary btn-lg">
+            Learn more
+          </Link>
+        </div>
+        <br />
+        <div>
+          <Select
+            defaultValue={options[0]}
+            options={options}
+            value={lang}
+            onChange={this.changeLang}
+            className="App-Select"
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withNamespaces("translations")(HomePage);
